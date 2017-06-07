4b60cec0e1b2a62e9ddd985ac7e52c0b9033685c
Dawid Drozd <dawid.drozd@playsoft.fr>
2017-06-07 15:28:08 +0200

    Add cocos2d-x-v2 Layout::Linear helper implementation

---
+++ wrapper/cocos2d-x-v2/crosslayout/layout/CocosLinear.h
@@ -0,0 +1,89 @@
+//
+// Created by Dawid Drozd aka Gelldur on 07.06.17.
+//
+#pragma once
+
+#include <base_nodes/CCNode.h>
+
+#include <crosslayout/layout/Linear.h>
+
+namespace CrossLayout
+{
+namespace Layout
+{
+
+//template<class T>
+class CocosLinear
+{
+public:
+	using Rect = Linear::Rect;
+	using Size = Linear::Size;
+	using Point =Linear::Point;
+	using Wrap = CrossLayout::ComposerCocos::Wrap;
+	using Orientation = Linear::Orientation;
+	using Gravity = Linear::Gravity;
+
+	Linear linear;
+
+	CocosLinear(cocos2d::CCNode* parent
+				, Linear::Orientation orientation = Linear::Orientation::horizontal
+				, int gravity = (Linear::Gravity::left | Linear::Gravity::top))
+			: linear(orientation, gravity)
+			, _parent(parent)
+	{
+	}
+
+	CocosLinear(cocos2d::CCNode* parent, Size desiredSize
+				, Linear::Orientation orientation = Linear::Orientation::horizontal
+				, int gravity = (Linear::Gravity::left | Linear::Gravity::top))
+			: linear(orientation, gravity)
+			, _parent(parent)
+			, _size(desiredSize)
+	{
+	}
+
+	void add(cocos2d::CCNode* child, Margin margin = {})
+	{
+		_parent->addChild(child);
+		linear.addItem<Wrap>(child, margin);
+	}
+
+	template<class N>
+	void nest(N& layout)
+	{
+		linear.nest<N>(layout);
+	}
+
+	void layout()
+	{
+		layout(getSize());
+	}
+
+	void layout(Size size)
+	{
+		linear.layout(size);
+	}
+
+	NodeGroup getNodePack()
+	{
+		return linear.getNodePack();
+	}
+
+	Size getSize()
+	{
+		return _size == Size{} ? _parent->getContentSize() : _size;
+	}
+
+	//Helper method
+	bool pleaseConsiderMeAsLayout()
+	{
+		return true;
+	}
+
+private:
+	cocos2d::CCNode* _parent = nullptr;
+	Size _size;
+};
+
+}
+}
